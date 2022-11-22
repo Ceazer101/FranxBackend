58c8c70701c12d82da0d6f8406cd61bbd7fc6e76
chris <chri402s@stud.kea.dk>
2022-11-22 14:38:58 +0100

    Initial commit

---
+++ src/main/java/com/example/franxbackend/FranxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.franxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FranxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/franxbackend/FranxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.franxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
