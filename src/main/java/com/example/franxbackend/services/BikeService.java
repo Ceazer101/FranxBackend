f04f9824e98dfc51ce399338cf13a899b2c76cf9
gulernurdonmez <gule0153@stud.kea.dk>
2022-11-23 13:17:13 +0100

    made addBike method

--- src/main/java/com/example/franxbackend/services/BikeService.java
+++ src/main/java/com/example/franxbackend/services/BikeService.java
@@ -1,4 +1,21 @@
 package com.example.franxbackend.services;
 
+import com.example.franxbackend.dtos.BikeRequest;
+import com.example.franxbackend.dtos.BikeResponse;
+import com.example.franxbackend.entities.Bike;
+import com.example.franxbackend.repositories.BikeRepository;
+
 public class BikeService {
+
+    BikeRepository bikeRepository;
+
+    public  BikeService(BikeRepository bikeRepository){
+        this.bikeRepository = bikeRepository;
+    }
+
+    public BikeResponse addBike(BikeRequest bikeRequest){
+        Bike newBike = BikeRequest.getBikeEntity(bikeRequest);
+        newBike = bikeRepository.save(newBike);
+        return new BikeResponse(newBike);
+    }
 }
